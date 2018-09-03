4aecd939c06276762f7477bf3575da11e24ae626
Pedro558 <pedroafonso109@gmail.com>
2018-09-02 22:14:07 -0300

    Creating Students table

--- Class_Project.sql
+++ Class_Project.sql
@@ -3,3 +3,23 @@ default character set utf8
 default collate utf8_general_ci;
 
 use class;
+
+create table students(
+	id_student int not null auto_increment,
+    student_name varchar(30),
+    nationality varchar(20),
+    city varchar(30),
+    phone int(15),
+    date_of_birth date,
+    sex enum ('M', 'F'),
+    profession varchar(30),
+    height decimal (3,2),
+    weight decimal (5,2),
+    primary key (id_student)
+)default charset = utf8;
+
+desc students;
+
+alter table students drop phone;
+alter table students modify profession varchar(20);
+
